263bad363e93b93b0a2db5c58a38cb329db5f0de
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2021-12-02 18:58:15 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function adduser() {
     localStorage.setItem("player1_name",player1_name);
     localStorage.setItem("player2_name",player2_name);
 
-    window.location.replace("game.html");
+    window.location="gameroom.html"
 }
